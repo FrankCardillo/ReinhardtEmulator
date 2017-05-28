d3f4166c42fba5e400592e9f88cc45e9b87664bf
Frank Cardillo <facardillo91@gmail.com>
2017-05-28 16:51:46 -0700

    further implementing/refining DRF

--- EmulationApplication/urls.py
+++ EmulationApplication/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^users/$', views.user_list, name='users'),
-    url(r'^user/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^$', views.index),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
